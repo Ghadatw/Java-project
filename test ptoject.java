fb2b62a057dcb6994b4e8ebb09ab9abd7f5c0d22
Ghadatw <162672289+Ghadatw@users.noreply.github.com>
2024-03-16 21:44:52 +0300

    Update test ptoject.java

--- test ptoject.java
+++ test ptoject.java
@@ -1,2 +1,11 @@
 public class test project{
 }
+
+
+public String Programmer(Engineer e) {
+e.setName("Eng. Ghada") ;
+KSU("SWE", e) ;
+if ( e.coding == true && e.coffee == true )
+String happiness = "infinity" ;
+return happiness ;
+}
